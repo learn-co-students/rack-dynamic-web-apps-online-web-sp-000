e6f576367ef768ada55116454e2063f18d094f9f
Ramon Chavez <rachavez1974@gmail.com>
2019-07-02 15:48:28 -0400

    Finish rack-dynamic-web-apps-online-web-sp-000

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    
+    resp.write "#{num_1 = Kernel.rand(1..20)}\n"
+    resp.write "#{num_2 = Kernel.rand(1..20)}\n"
+    resp.write "#{num_3 = Kernel.rand(1..20)}\n"
+
+    resp.write (num_1 == num_2 && num_2 == num_3) ? "You Win" : "You Lose" 
+
     resp.finish
   end
 
